5653997283a5b73ced15b2b3889a698589faa6da
Лавров Сергей <lavrov-sergey@yandex.ru>
2020-05-22 07:16:54 +0300

    Добавлен вывод сообщения на каждое действие из меню кроме отмены

--- 03/Alert/Alert/ViewController.swift
+++ 03/Alert/Alert/ViewController.swift
@@ -18,35 +18,43 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showButton(_ sender: Any) {
-        self.alert(title: "Внимание", message: "Введите логин и пароль")
+        self.alert(title: "Внимание", message: "Введите логин и пароль", auth: true)
     }
     
     @IBAction func showSheet(_ sender: Any) {
         self.menu()
     }
     
-    func alert(title: String, message: String){
+    func alert(title: String, message: String, auth: Bool){
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "ok", style: .default) { (action) in
-            let text = alertController.textFields?.first?.text
-            if(text == ""){
-                self.hiLabel.text = "Привет!"
-            } else {
-                self.hiLabel.text = "Привет, " + (text!) + "!"
+            if (auth){
+                let text = alertController.textFields?.first?.text
+                if(text == ""){
+                    self.hiLabel.text = "Привет!"
+                } else {
+                    self.hiLabel.text = "Привет, " + (text!) + "!"
+                }
+            }
+        }
+        if (auth){
+            alertController.addTextField()
+            alertController.addTextField{ (passwordField) in
+                passwordField.isSecureTextEntry = true
             }
         }
         alertController.addAction(action)
-        alertController.addTextField()
-        alertController.addTextField{ (passwordField) in
-            passwordField.isSecureTextEntry = true
-        }
         self.present(alertController, animated: true, completion: nil)
     }
     
     func menu(){
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "Один", style: .default))
-        alertController.addAction(UIAlertAction(title: "Два", style: .default))
+        alertController.addAction(UIAlertAction(title: "Один", style: .default){(action) in
+            self.alert(title: "Один", message: "Первое действие", auth: false)
+        })
+        alertController.addAction(UIAlertAction(title: "Два", style: .default){(action) in
+            self.alert(title: "Два", message: "Второе действие", auth: false)
+        })
         alertController.addAction(UIAlertAction(title: "Отмена", style: .cancel))
         self.present(alertController, animated: true, completion: nil)
     }
