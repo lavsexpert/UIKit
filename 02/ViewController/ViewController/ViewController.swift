ab714187dfc1532dd9c97786bdafbc751df3630b
Лавров Сергей <lavrov-sergey@yandex.ru>
2020-05-22 05:06:50 +0300

    Созданы: экран, ViewController, события жизненного цикла UIViewController

---
+++ 02/ViewController/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewController
+//
+//  Created by Sergey Lavrov on 22.05.2020.
+//  Copyright © 2020 +1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
