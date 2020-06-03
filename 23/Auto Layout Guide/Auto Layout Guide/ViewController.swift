c524e93c648aa8e42192abf1ec98f8f24dcf2229
Лавров Сергей <lavrov-sergey@yandex.ru>
2020-06-04 02:03:29 +0300

    Auto Layout Guide: Настроили привязки для 1 view и для 2х view

---
+++ 23/Auto Layout Guide/Auto Layout Guide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Guide
+//
+//  Created by Sergey Lavrov on 04.06.2020.
+//  Copyright © 2020 +1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
